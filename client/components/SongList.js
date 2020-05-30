700b976c0d09c053b3929bf00552dbbe5b149cfb
Enzo Esperguin <enzoesper@hotmail.com>
2020-05-30 10:40:16 -0300

    modifying the way that the songlist is refetched once a song is deleted

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,12 +6,8 @@ import query from '../queries/fetchSongs';
 class SongList extends React.Component {
   
   onSongDelete(id) {
-    this.props.mutate({
-      variables: {
-        id: id
-      },
-      refetchQueries: [{ query: query }]
-    })
+    this.props.mutate({ variables: { id: id } })
+      .then(() => this.props.data.refetch());
   }
 
   renderSongs() {

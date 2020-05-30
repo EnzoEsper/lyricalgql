1f3b64b76264bd91a6bf05cba6b1ed3d06b991c5
Enzo Esperguin <enzoesper@hotmail.com>
2020-05-30 10:52:56 -0300

    creating a component to show the details for a particular song

---
+++ client/components/SongDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SongDetail extends React.Component {
+  render() {
+    return(
+      <div>
+        <h3>Song Detail</h3>
+      </div>
+    )
+  }
+}
+
+export default SongDetail;
